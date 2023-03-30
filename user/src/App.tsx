d0f4eea13c8af3af9e675618e328ed5d3325b7fa
piyush wandile <piyushwandile134@gmail.com>
2023-03-30 23:32:37 +0530

    second commit

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { Route } from "@mui/icons-material";
+import { CssBaseline } from "@mui/material";
+import { Box } from "@mui/system";
+import { createTheme } from "@mui/system";
+import { useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { themeSettings } from "./theme";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route></Route>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
